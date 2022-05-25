0cc60b058fa40189bc452ba2af2177b7aab1d924
nur.iman <nur.iman@kalsym.com>
2022-05-25 11:49:46 +0800

    fix build problem

--- src/main/java/com/kalsym/locationservice/repository/CategoryRepository.java
+++ src/main/java/com/kalsym/locationservice/repository/CategoryRepository.java
@@ -1,19 +1,14 @@
 package com.kalsym.locationservice.repository;
 
 import java.util.List;
-import java.util.Map;
 
 import com.kalsym.locationservice.model.Category;
-import com.kalsym.locationservice.model.ParentCategory;
 
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import javafx.util.Pair;  
+
 
 
 @Repository
