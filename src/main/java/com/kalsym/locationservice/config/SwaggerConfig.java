3352ae441c30cecbb19a73166ec172cf5cb74af8
taufik <taufik@kalsym.com>
2022-11-07 11:52:19 +0800

    update version in swagger

--- src/main/java/com/kalsym/locationservice/config/SwaggerConfig.java
+++ src/main/java/com/kalsym/locationservice/config/SwaggerConfig.java
@@ -57,7 +57,7 @@ public class SwaggerConfig {
                 .description("Used to manage filtering based on location")
                 .termsOfServiceUrl("not added yet")
                 .license("not added yet")
-                .licenseUrl("").version("1.0.40").build();
+                .licenseUrl("").version("1.1.0").build();
     }
 
     public static final String DEFAULT_INCLUDE_PATTERN = "/.*";
